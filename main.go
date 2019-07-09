eba3bd8298c63199e58f39c75a173a4f63af139d
Johnny Tardin <johnnytardin@users.noreply.github.com>
2019-07-09 20:43:36 -0300

    Criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package area
+
+import "math"
+
+// Pi é uma proporção numérica definida  pela relação entre o perímetro
+// de uma circunferência e o seu diâmetro
+const Pi = 3.1416
+
+// Circ é reponsável por calcular a aŕea da circunferência
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// Rect é responsável por calcular a área de um retângulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+// Não é visível
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
